c5e354e43d112aa6fe5eae6463d52895e9c5b1e8
donghyeon2 <tsvz1234@gmail.com>
2017-07-29 16:37:01 +0900

    arp header, function bug fix

--- arp_send.c
+++ arp_send.c
@@ -5,24 +5,20 @@
 #include <net/ethernet.h>
 #include <pcap/pcap.h>
 #include <string.h>
+#include <net/if_arp.h>
 
 #define p_size 128
-
+#define IPV4_ADDR_LEN 4
+#define HARD_TYPE 6
 char packet[p_size];
 
+struct mac_ip{
+	u_char s_mac[6];
+	u_char s_ip[4];
+	u_char d_mac[6];
+	u_char d_ip[4];
+}__attribute((__packed__));
 char* send_arp(char *s_ip, char *d_ip);
-
-struct arp_header{
-	u_short hard_type; //2 byte//
-	u_short procol_type; //2 byte//
-	u_char hard_size; //1 byte//
-	u_char procol_size; //1 byte//
-	u_short operation; //2 byte//
-	u_char s_mac[6]; //6 byte//
-	u_char s_ip[INET_ADDRSTRLEN]; //16 byte//
-	u_char d_mac[6];//6 byte//
-	u_char d_ip[INET_ADDRSTRLEN];//16 byte//
-}__attribute__((packed));
 
 int main(int argc, char* argv[]){
 	pcap_t *fd;
@@ -41,36 +37,39 @@ int main(int argc, char* argv[]){
 	printf("\t BEST OF THE BEST 6TH ARP PACKET SENDER \t \n");
 	printf("\t BY donghyeon2 \t \n");
 	send_arp(argv[2], argv[3]);
-	pcap_sendpacket(fd, packet, sizeof(packet));
+	pcap_sendpacket(fd, packet, 42);
 	return 0;
 }
 
 char *send_arp(char *s_ip, char*d_ip){
 	struct ether_header *ep;
-	struct arp_header *arp;
 	ep = (struct ether_header*)packet;
-	arp = (struct arp_header*)packet+14;
-	for(int i=0; i<6; i++){
+	struct arphdr *arph;
+	arph = (struct arphdr*)packet+sizeof(ep);
+	struct mac_ip *adr;
+	adr = (struct mac_ip*)packet+sizeof(ep)+sizeof(arph);
+	for (int i = 0; i <6; i++)
+	{
 		ep->ether_dhost[i] = 0xff;
 	}
 	ep->ether_shost[0] = 0x00;
-	ep->ether_shost[1] = 0x0c;
-	ep->ether_shost[2] = 0x29;
-	ep->ether_shost[3] = 0x53;
-	ep->ether_shost[4] = 0xd5;
-	ep->ether_shost[5] = 0x90;
+	ep->ether_shost[1] = 0x1c;
+	ep->ether_shost[2] = 0x42;
+	ep->ether_shost[3] = 0x8d;
+	ep->ether_shost[4] = 0x79;
+	ep->ether_shost[5] = 0xce;
 	ep->ether_type = ntohs(ETHERTYPE_ARP);
 	memcpy(packet, ep, sizeof(ep));
-	arp->hard_type = 0x01;
-	arp->procol_type = ETHERTYPE_IP;
-	arp->hard_size = 0x06;
-	arp->procol_size = 0x04;
-	arp->operation = 0x01;
-	memcpy(arp->s_mac, ep->ether_shost, 6);
-	inet_pton(AF_INET, s_ip, arp->s_ip);
-	memcpy(arp->d_mac, ep->ether_dhost, 6);
-	inet_pton(AF_INET, d_ip, arp->d_ip);
-	memcpy(packet+14, arp, sizeof(arp));
-	memset(packet+66, 0, sizeof(packet));
-	return packet;
+	arph->ar_hrd = htons(ARPHRD_ETHER);
+	arph->ar_pro = htons(ETHERTYPE_IP);
+	arph->ar_hln = 0x06;
+	arph->ar_pln = 0x04;
+	arph->ar_op = htons(ARPOP_REQUEST);
+	memcpy(packet+14, arph, 8);
+	memcpy(adr->s_mac, ep->ether_shost, 6);
+	inet_pton(AF_INET, s_ip, adr->s_ip);
+	memcpy(adr->s_mac, ep->ether_dhost, 6);
+	inet_pton(AF_INET, d_ip, adr->d_ip);
+	memcpy(packet+22, adr, 20);
+	printf("%x\n", packet[20]);
 }
